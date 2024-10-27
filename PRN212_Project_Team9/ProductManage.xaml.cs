b43949e5477da601394047c0a0a47a3560670aba
ttrungpthe170431 <trungpthe170431@fpt.edu.vn>
2024-10-27 21:53:10 +0700

    fi
    fi

--- PRN212_Project_Team9/ProductManage.xaml.cs
+++ PRN212_Project_Team9/ProductManage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PRN212_Project_Team9.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,47 @@ namespace PRN212_Project_Team9
     /// </summary>
     public partial class ProductManage : Window
     {
+        SalesManagementDbContext db = new SalesManagementDbContext();
         public ProductManage()
         {
             InitializeComponent();
+            LoadLvProduct();
+            LoadCbCategory();
+        }
+
+        public void LoadLvProduct()
+        {
+            var listProduct = db.Products.Select(x => new
+            {
+                ProductID = x.ProductId,
+                ProductName = x.ProductName,
+                Category = x.Category.CategoryName,
+                Price = x.Price,
+                StockQuantity = x.StockQuantity,
+                Description = x.Description,
+            }).ToList();
+            lvProduct.ItemsSource = listProduct;
+        }
+
+        public void LoadCbCategory()
+        {
+            var listCategory = db.Categories.Select(x => x.CategoryName).ToList();
+            cbCategory.ItemsSource = listCategory;
+            cbCategory.SelectedIndex = 0;
+        }
+
+        private void lvProduct_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedProduct = lvProduct.SelectedItem as dynamic;
+            if (selectedProduct != null)
+            {
+                txtProductID.Text = selectedProduct.ProductID.ToString();
+                txtProductName.Text = selectedProduct.ProductName;
+                cbCategory.Text = selectedProduct.Category;
+                txtPrice.Text = selectedProduct.Price.ToString();
+                txtStockQuantity.Text = selectedProduct.StockQuantity.ToString();
+                txtDescription.Text = selectedProduct.Description;
+            }
         }
     }
 }
