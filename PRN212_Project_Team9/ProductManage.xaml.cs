c16ddb188afb3183a97ebcabb25a8cef69af0513
ttrungpthe170431 <trungpthe170431@fpt.edu.vn>
2024-10-27 19:55:10 +0700

    f
    f

---
+++ PRN212_Project_Team9/ProductManage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PRN212_Project_Team9
+{
+    /// <summary>
+    /// Interaction logic for ProductManage.xaml
+    /// </summary>
+    public partial class ProductManage : Window
+    {
+        public ProductManage()
+        {
+            InitializeComponent();
+        }
+    }
+}
