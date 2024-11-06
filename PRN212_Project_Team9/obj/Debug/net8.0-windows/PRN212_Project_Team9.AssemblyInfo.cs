0dac55f32a1b75270af8ffc9c684e78ea71992ef
MinhTanTMT <tantmhe161872@fpt.edu.vn>
2024-11-06 15:25:06 +0700

    CodeCommit: 679485

--- PRN212_Project_Team9/obj/Debug/net8.0-windows/PRN212_Project_Team9.AssemblyInfo.cs
+++ PRN212_Project_Team9/obj/Debug/net8.0-windows/PRN212_Project_Team9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN212_Project_Team9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a590dbeafd460897d59c9bec9a507b100353bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf58f379dd9a612fda7fabae7b2b817bb462bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN212_Project_Team9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN212_Project_Team9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
