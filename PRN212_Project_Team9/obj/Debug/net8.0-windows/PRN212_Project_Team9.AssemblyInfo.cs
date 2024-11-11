80497c7e5c91faaecef595b0f123f95e004b09b9
MinhTanTMT <132992493+MinhTanTMT@users.noreply.github.com>
2024-11-11 20:39:05 +0700

    CodeComplete

--- PRN212_Project_Team9/obj/Debug/net8.0-windows/PRN212_Project_Team9.AssemblyInfo.cs
+++ PRN212_Project_Team9/obj/Debug/net8.0-windows/PRN212_Project_Team9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN212_Project_Team9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290c7b2dd2dc53f5ddf4e60fcb367612e6d616ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bb6bca4d0f5bd91820b1d34b82396c178b24c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN212_Project_Team9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN212_Project_Team9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
